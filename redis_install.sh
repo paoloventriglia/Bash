8234f7901218a9c653a4b743c79fa3028eb6b526
Paolo Ventriglia <paolo@corebox.co.uk>
2018-02-09 11:42:12 +0000

    Added comments

--- redis_install.sh
+++ redis_install.sh
@@ -1,11 +1,35 @@
+# Run yum update and install make and gcc
+# yum update && yum install -y make gcc
+
+# Unpack source code files
 tar xvzf redis-3.2.8.tar.gz
+
+# Change directory to redis
 cd redis-3.2.8
+
+# Compile the src code file, this will require oyu have make and gcc installed
 make
+
+# Make install will copy the executable to the relevant locations
 make install
+
+# Make redis folder for config file
 mkdir /etc/redis
-mkdir /var/redis
-cp redisd /etc/init.d/redisd
-cp redis.conf /etc/redis/redis.conf
+
+# Create working directory
 mkdir /var/redis/redisd
+
+# Copy init script file from utils to init.d 
+cp utils/redisd /etc/init.d/redisd
+
+# Copy redis config file /etc/redis
+cp redis.conf /etc/redis/redis.conf
+
+# Add redis service to chkconfig
 chkconfig --add redisd
+
+# Enable redis service to start at startup
 chkconfig redisd on
+
+# Make redis service script executable
+chmod +x  /etc/init.d/redisd
