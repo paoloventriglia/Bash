da71e9c97316e9e7f8075704c46939db33e78629
Paolo Ventriglia <paolo@corebox.co.uk>
2018-05-02 17:08:22 +0100

    added backup scripts

---
+++ backup_files_02.py
@@ -0,0 +1,12 @@
+#__author__ = "Paolo Ventriglia"
+#__credits__ = ["Paolo Ventriglia"]
+#__license__ = "GPL"
+#__version__ = ""
+#__maintainer__ = "Paolo Ventriglia"
+#__email__ = "paolo@corebox.co.uk"
+
+'''Please insert program scope'''
+
+# import modules
+# set variables
+
